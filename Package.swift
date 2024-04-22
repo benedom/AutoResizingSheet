f80cd70aedd31d6fd5987bd7ea834fa7d53cc7ad
benedom <31181527+benedom@users.noreply.github.com>
2024-04-22 09:29:46 +0200

    Add PrivacyInfo file

--- Package.swift
+++ Package.swift
@@ -16,7 +16,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AutoResizingSheet"),
+            name: "AutoResizingSheet",
+            path: "Sources",
+            resources: [.copy("PrivacyInfo.xcprivacy")]
+        ),
         .testTarget(
             name: "AutoResizingSheetTests",
             dependencies: ["AutoResizingSheet"]),
