e88f016f078e2d060bcf824e60b44bbe275be6b1
benedom <31181527+benedom@users.noreply.github.com>
2024-05-02 21:40:44 +0200

    Fix tests

--- Tests/AutoResizingSheetTests/AutoResizingSheetTests.swift
+++ Tests/AutoResizingSheetTests/AutoResizingSheetTests.swift
@@ -6,12 +6,10 @@ final class AutoResizingSheetTests: XCTestCase {
         let configuration = AutoResizingSheetConfiguration(
             scrollable: false,
             showGrabber: false,
-            initialDetent: .large(),
             extendableToFullSize: false
         )
         XCTAssertEqual(configuration.scrollable, false)
         XCTAssertEqual(configuration.showGrabber, false)
-        XCTAssertEqual(configuration.initialDetent, UISheetPresentationController.Detent.large())
         XCTAssertEqual(configuration.extendableToFullSize, false)
     }
 }
